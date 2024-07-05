21ca050882d32fb2edbebceaaba9f82a84ed1b63
Sahiljain24 <24sahilj@gmail.com>
2024-07-05 22:52:56 +0530

    update

--- Server/Controllers/Auth.js
+++ Server/Controllers/Auth.js
@@ -1,2 +1,16 @@
 
-async 
+exports.sendOtp = async(req,res)=>{
+    try{
+        const email =req.body;
+        const isAlreadyPresent = app.findOne(email);
+        if(!isAlreadyPresent){
+            console.log("user already Present");
+            res.json({
+
+            })
+        }
+    }
+    catch{
+
+    }
+}
