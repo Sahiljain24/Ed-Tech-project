c1116b9cc71b07934d78e6b03fe8b751ed0b7936
Sahiljain24 <24sahilj@gmail.com>
2024-07-23 23:02:14 +0530

    update

--- Server/Controllers/Auth.js
+++ Server/Controllers/Auth.js
@@ -1,3 +1,9 @@
+const otpGenerator= require("otp-generator");
+const OTP =require("../Models/Otp");
+
+
+
+
 
 exports.sendOtp = async(req,res)=>{
     try{
@@ -10,14 +16,36 @@ exports.sendOtp = async(req,res)=>{
             return res.status(401).json({
                 success:false,
                 message:'User already registered',
-                success:true;
+                success:true,
             })
         }
+        var otp = otpGenerator.generate(6,{
+            specialChars:false,
+            upperCaseAlphabets:false,
+            lowerCaseAlphabets:false,
+        });
+        const isUnique=await OTP.findOne({otp:otp});
+        while(!isUnique){
+            otp=otpGenerator.generate(6,{
+                specialChars:false,
+                upperCaseAlphabets:false,
+                lowerCaseAlphabets:false,
+            });
+            isUnique=await OTP.findOne({otp:otp});
         }
-    }
-    catch (err){
-        console.log("Could not send OTP");
-        res.json()
+        console.log("OTP is" + otp);
+        const otpPayload = {
+            email,otp
+        };
+        const otpBody = await OTP.create(otpPayload);
+
+
+
+
+        }
+ 
+    catch(err){
+        
     }
     
 }
