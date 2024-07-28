61c8b3a8db258e359f2b67551849313a8ad5992c
Sahiljain24 <24sahilj@gmail.com>
2024-07-28 22:28:45 +0530

    update

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,13 @@
-const express = require("express");
-const app = express();
-require("dotenv");
+// const express = require("express");
+// const app = express();
+// require("dotenv");
 
- app.get("/",homePage);
+//  app.get("/",homePage);
+
+ let str = "this is a sunday";
+
+ const newSTR= str.split(" ").map(element => 
+    (element.charAt(0).toUpperCase()+element.slice(1))).join(" ")
+ ;
+
+ console.log(newSTR);
